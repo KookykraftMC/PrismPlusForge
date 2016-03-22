3c25f34087670abef0e0b65b09c378ddeb0a6c57
Time6628 <pturchan@yahoo.com>
2016-03-22 17:18:46 -0400

    More Prism testing.

--- src/main/java/com/kookykraftmc/prismplus/forge/EventHookListener.java
+++ src/main/java/com/kookykraftmc/prismplus/forge/EventHookListener.java
@@ -1,14 +1,19 @@
 package com.kookykraftmc.prismplus.forge;
 
+import com.kookykraftmc.prismplus.customevents.TestEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 /**
  * Created by TimeTheCat on 3/21/2016.
  */
 public class EventHookListener {
     @SubscribeEvent
-    public void onTest(LivingDropsEvent event) {
-
+    public void onTest(PlayerEvent.PlayerLoggedInEvent event) {
+        Player p = (Player) event.player;
+        Bukkit.getServer().getPluginManager().callEvent(new TestEvent((p)));
     }
 }
--- src/main/java/com/kookykraftmc/prismplus/forge/ModInfo.java
+++ src/main/java/com/kookykraftmc/prismplus/forge/ModInfo.java
@@ -4,7 +4,7 @@ package com.kookykraftmc.prismplus.forge;
  */
 public class ModInfo {
     public static final String NAME = "PrismPlusForge";
-    public static final String ID = "prismplusforge";
+    public static final String ID = "PrismPlusForge";
     public static final String CHANNEL = "PPForge";
     public static final String DEPEND = "";
     public static final String VERSION = "0.5";
